da91c2f884c797842274d9836dc4cc81baf88804
maedana <maeda.na@gmail.com>
2022-02-13 11:31:34 +0900

    共通部分をDRYに

--- 07.bowling_object/frame_calculator.rb
+++ 07.bowling_object/frame_calculator.rb
@@ -5,12 +5,13 @@ class FrameCalculator
     @first_score = score(first_mark)
     @second_score = score(second_mark)
     @third_score = score(third_mark)
+    @first_plus_second_score = @first_score + @second_score
   end
 
   def result
-    return @first_score + @second_score if @first_score + @second_score < 10
+    return @first_plus_second_score if @first_plus_second_score < 10
 
-    @first_score + @second_score + @third_score
+    @first_plus_second_score + @third_score
   end
 
   private
