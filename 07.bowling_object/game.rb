9a2e332673ea0fb2472cb02194f0b918f853e767
maedana <maeda.na@gmail.com>
2022-02-13 12:08:49 +0900

    3行しかないローカル変数だし短くていいかなと

--- 07.bowling_object/game.rb
+++ 07.bowling_object/game.rb
@@ -9,9 +9,9 @@ class Game
 
   def result
     Array.new(10).sum do
-      frame_calculator = FrameCalculator.new(*@marks.slice(0, 3))
-      frame_calculator.strike? ? @marks.shift : @marks.shift(2)
-      frame_calculator.result
+      f = FrameCalculator.new(*@marks.slice(0, 3))
+      f.strike? ? @marks.shift : @marks.shift(2)
+      f.result
     end
   end
 end
