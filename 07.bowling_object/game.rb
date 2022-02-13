77dec1b8e0fda2f452a2218514019e37381f23dd
maedana <maeda.na@gmail.com>
2022-02-13 12:07:35 +0900

    一度のループでフレームごとの計算までやるように

--- 07.bowling_object/game.rb
+++ 07.bowling_object/game.rb
@@ -5,20 +5,13 @@ require_relative 'frame_calculator'
 class Game
   def initialize(mark_text)
     @marks = mark_text.split(',')
-    @frame_calculators = build_frame_caluculators
   end
 
   def result
-    @frame_calculators.map(&:result).sum
-  end
-
-  private
-
-  def build_frame_caluculators
-    Array.new(10) do
+    Array.new(10).sum do
       frame_calculator = FrameCalculator.new(*@marks.slice(0, 3))
       frame_calculator.strike? ? @marks.shift : @marks.shift(2)
-      frame_calculator
+      frame_calculator.result
     end
   end
 end
