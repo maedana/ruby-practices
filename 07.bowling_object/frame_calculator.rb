803fa5661e6038294001946deb546876f4cb0e87
maedana <maeda.na@gmail.com>
2022-02-18 17:07:57 +0900

    initializerでしか使わないのでローカル変数に

--- 07.bowling_object/frame_calculator.rb
+++ 07.bowling_object/frame_calculator.rb
@@ -6,9 +6,9 @@ class FrameCalculator
 
   def initialize(first_mark, second_mark, third_mark = nil)
     @first_score = score(first_mark)
-    @second_score = score(second_mark)
+    second_score = score(second_mark)
     @third_score = third_mark ? score(third_mark) : 0
-    @first_two_score = @first_score + @second_score
+    @first_two_score = @first_score + second_score
   end
 
   def result
